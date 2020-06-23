c4c2cebc5d9d073e719f53de8e13c45a23ef2c14
Yannick Widmer <yannick.widmer@math.uzh.ch>
2020-06-23 10:04:10 +0200

    use empty default theme

--- lv_conf.h
+++ lv_conf.h
@@ -383,12 +383,12 @@ typedef void * lv_font_user_data_t;
  *================*/
 
 /*Always enable at least on theme*/
-#define LV_USE_THEME_EMPTY       0   /*No theme, you can apply your styles as you need*/
+#define LV_USE_THEME_EMPTY       1   /*No theme, you can apply your styles as you need*/
 #define LV_USE_THEME_TEMPLATE    0   /*Simple to the create your theme based on it*/
 #define LV_USE_THEME_MATERIAL    1   /*A fast and impressive theme*/
 #define LV_USE_THEME_MONO        0   /*Mono-color theme for monochrome displays*/
 
-#define LV_THEME_DEFAULT_INIT               lv_theme_material_init
+#define LV_THEME_DEFAULT_INIT               lv_theme_empty_init
 #define LV_THEME_DEFAULT_COLOR_PRIMARY      LV_COLOR_RED
 #define LV_THEME_DEFAULT_COLOR_SECONDARY    LV_COLOR_BLUE
 #define LV_THEME_DEFAULT_FLAG               LV_THEME_MATERIAL_FLAG_LIGHT
